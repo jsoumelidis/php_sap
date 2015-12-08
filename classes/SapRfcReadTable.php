d9414568c93f27454f3384c2c128d1d96dce5ed8
jsoumelidis <jsoumelidis@gmail.com>
2015-12-08 20:11:07 +0200

    doc update

--- classes/SapRfcReadTable.php
+++ classes/SapRfcReadTable.php
@@ -7,7 +7,9 @@ class SapRfcReadTable extends SapFunction
 	 * select() method simulates the SQL "SELECT 'fields' FROM 'table' WHERE 'options' LIMIT 'limit' OFFSET 'offset'"
 	 * to read data directly from a SAP database table.
 	 * Param $fields should be a string (select one field only), or '*' (SQL STAR constant) for all table's fields,
-	 * or an array containing the fields of the table that you are interested (empty array is equal to SQL star '*').
+	 * or an array containing the fields of the table that you are interested. For every item in $fields array with
+	 * a string key, an alias will be created for this item's value (alias => fieldname) and the result array will
+	 * export 'fieldname' as 'alias' like the "SELECT fieldname AS alias ..." SQL syntax
 	 * Every item in $options with a string key will be interpreted as 'FIELDNAME' => 'FIELDVALUE'
 	 * and a corresponding "AND FIELDNAME EQ 'FIELDVALUE'" clause will be appended to function's OPTIONS table.
 	 * If key is integer then the value is assumed as a custom 'WHERE' clause and is appended as is.
@@ -15,7 +17,7 @@ class SapRfcReadTable extends SapFunction
 	 * All $options array values will be converted to strings.
 	 * Be aware of quotes in $options array values, they must be escaped.
 	 * Each $options array value should be at most 72 characters long (as limited by the RFC_READ_TABLE function)
-	 * Sum of defined fields length should be at most 512 characters long (as limited by the RFC_READ_TABLE function)
+	 * Total length of requested fields should not exceed 512 characters (as limited by the RFC_READ_TABLE function)
 	 *
 	 * @param array|string $fields
 	 * @param string $table
