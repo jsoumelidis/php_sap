05779a74ebe1bc0eae1b2d564a1d036c409625f2
Soumelidis, Ioannis <jsoumelidis@gmail.com>
2018-03-27 22:01:20 +0300

    Added trace_dir and trace_level ini settings

--- php_sap.c
+++ php_sap.c
@@ -91,7 +91,9 @@ typedef struct _sap_object {
 } sap_object;
 
 ZEND_BEGIN_MODULE_GLOBALS(sap)
-    int rtrim_export_strings;
+    zend_bool rtrim_export_strings;
+    char *trace_dir;
+    zend_long trace_level;
     char *sapnwrfc_ini_dir;
 ZEND_END_MODULE_GLOBALS(sap)
 
@@ -100,7 +102,13 @@ ZEND_DECLARE_MODULE_GLOBALS(sap)
 static ZEND_MODULE_GLOBALS_CTOR_D(sap)
 {
     sap_globals->rtrim_export_strings = 0;
+    sap_globals->trace_dir = NULL;
+    sap_globals->trace_level = 0;
     sap_globals->sapnwrfc_ini_dir = NULL;
+}
+
+static ZEND_MODULE_GLOBALS_DTOR_D(sap)
+{
 }
 
 #ifdef SAPwithPTHREADS
@@ -143,7 +151,11 @@ zend_module_entry sap_module_entry = {
     NULL, /* PHP_RSHUTDOWN(php_sap), */
     PHP_MINFO(sap),
     PHP_SAP_VERSION,
-    STANDARD_MODULE_PROPERTIES
+    ZEND_MODULE_GLOBALS(sap),
+    ZEND_MODULE_GLOBALS_CTOR_N(sap),
+    ZEND_MODULE_GLOBALS_DTOR_N(sap),
+    NULL,
+    STANDARD_MODULE_PROPERTIES_EX
 };
 
 #ifdef COMPILE_DL_SAP
@@ -3201,7 +3213,74 @@ PHP_INI_MH(OnUpdateSapNwRfcIniDir)
     return SUCCESS;
 }
 
+PHP_INI_MH(OnUpdateSapNwTraceDir)
+{
+    SAPRFC_ERROR_INFO err;
+    SAP_UC *uTracePath;
+    unsigned int uTracePathLen;
+
+    //Default ini string handling
+    OnUpdateString(entry, new_value, mh_arg1, mh_arg2, mh_arg3, stage);
+
+    if (new_value && SUCCESS == utf8_to_sapuc_l(ZSTR_VAL(new_value), ZSTR_LEN(new_value), &uTracePath, &uTracePathLen, &err))
+    {
+        if (RFC_OK != RfcSetTraceDir(uTracePath, (RFC_ERROR_INFO*)&err))
+        {
+            char *message;
+            int messageLen;
+            SAPRFC_ERROR_INFO e;
+
+            if (SUCCESS == sapuc_to_utf8_l((SAP_UC*)&err.err.message, sizeof(err.err.message), &message, &messageLen, &e)) {
+                php_error(E_WARNING, "Could not set trace path: %s", message);
+            }
+        }
+
+        efree(uTracePath);
+    }
+
+    return SUCCESS;
+}
+
+PHP_INI_MH(OnUpdateSapNwTraceLevel)
+{
+    RFC_ERROR_INFO err;
+    unsigned int traceLevel;
+
+    //Default ini long handling
+    if (SUCCESS != OnUpdateLongGEZero(entry, new_value, mh_arg1, mh_arg2, mh_arg3, stage)) {
+        return FAILURE;
+    }
+
+    if (!new_value) {
+        return SUCCESS;
+    }
+
+    if (PHP_SAP_GLOBALS(trace_level) > 3)
+    {
+        php_error(E_WARNING, "Invalid trace level (%d)", PHP_SAP_GLOBALS(trace_level));
+        return FAILURE;
+    }
+
+    traceLevel = PHP_SAP_GLOBALS(trace_level);
+
+    if (RFC_OK != RfcSetTraceLevel(NULL, NULL, traceLevel, &err))
+    {
+        char *message;
+        int messageLen;
+        SAPRFC_ERROR_INFO e;
+
+        if (SUCCESS == sapuc_to_utf8_l((SAP_UC*)&err.message, sizeof(err.message), &message, &messageLen, &e)) {
+            php_error(E_WARNING, "Could not set trace level to %d: %s", traceLevel, message);
+        }
+    }
+
+    return SUCCESS;
+}
+
 PHP_INI_BEGIN()
+    /* always parse the trace dir first (?) */
+    STD_PHP_INI_ENTRY("sap.trace_dir", NULL, PHP_INI_ALL, OnUpdateSapNwTraceDir, trace_dir, zend_sap_globals, sap_globals)
+    STD_PHP_INI_ENTRY("sap.trace_level", NULL, PHP_INI_ALL, OnUpdateSapNwTraceLevel, trace_level, zend_sap_globals, sap_globals)
     STD_PHP_INI_ENTRY("sap.sapnwrfc_ini_dir", NULL, PHP_INI_ALL, OnUpdateSapNwRfcIniDir, sapnwrfc_ini_dir, zend_sap_globals, sap_globals)
     STD_PHP_INI_ENTRY("sap.rtrim_export_strings", "Off", PHP_INI_ALL, OnUpdateBool, rtrim_export_strings, zend_sap_globals, sap_globals)
 PHP_INI_END()
